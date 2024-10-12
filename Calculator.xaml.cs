d36bc4c81934d5f9ced89d866c2c29b94948ecf7
sohail-eng <74501059+sohail-eng@users.noreply.github.com>
2024-10-12 12:37:16 +0500

    Add project files.

---
+++ Calculator.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace class_work_wpf
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class Calculator : Page
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            answer.Text = (Convert.ToDouble(number1.Text) + Convert.ToDouble(number2.Text)).ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            answer.Text = (Convert.ToDouble(number1.Text) - Convert.ToDouble(number2.Text)).ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            answer.Text = (Convert.ToDouble(number1.Text) * Convert.ToDouble(number2.Text)).ToString();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            answer.Text = (Convert.ToDouble(number1.Text) / Convert.ToDouble(number2.Text)).ToString();
+        }
+    }
+}
---
+++ login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace class_work_wpf
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Page
+    {
+        Calculator c;
+        MainWindow window;
+        public login(Calculator c, MainWindow window)
+        {
+            InitializeComponent();
+            this.c = c;
+            this.window = window;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (username.Text == "admin" && password.Text == "admin")
+            {
+                MessageBox.Show("Success!");
+                window.mainview.Content = c;
+            }
+        }
+    }
+}
